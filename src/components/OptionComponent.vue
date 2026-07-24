21710339ce0a86ef78ec8c06f4b496eaa87a3adc
Isaac Castillo <iocastillo@innovacion.gob.sv>
2026-07-24 08:12:42 -0600

    fix: clean up markup and adjust z-index in OptionComponent

--- src/components/OptionComponent.vue
+++ src/components/OptionComponent.vue
@@ -5,7 +5,7 @@
     :style="{ width: '200px', height: '200px', left: `${props.x}px`, top: `${props.y}px` }"
   >
     <li role="menuitem" tabindex="0" aria-haspopup="true">View</li>
-    <li role="menuitem" tabindex="0" aria-haspopup="true" >
+    <li role="menuitem" tabindex="0" aria-haspopup="true">
       Sort by
       <ul role="menu">
         <li role="menuitem"><a href="#menu">Name</a></li>
@@ -21,22 +21,15 @@
 </template>
 
 <script setup lang="ts">
-
 const props = defineProps<{
   x: number
   y: number
 }>()
-
-
-
-
-
-
 </script>
 
 <style scoped>
 .option-menu {
   position: absolute;
-  z-index: 1000;
+  z-index: 10;
 }
 </style>
